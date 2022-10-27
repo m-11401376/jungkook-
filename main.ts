502b77ea7f7b9a743ca03b9b5bd25e8c08ac155f
m-11401376 <116236787+m-11401376@users.noreply.github.com>
2022-10-27 10:03:43 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,4 @@
 basic.forever(function () {
-    if (input.lightLevel() < 80) {
-        pins.servoWritePin(AnalogPin.P2, 180)
-    } else {
-        pins.servoWritePin(AnalogPin.P2, 0)
-    }
-})
-basic.forever(function () {
     if (true) {
         pins.digitalWritePin(DigitalPin.P0, 0)
         basic.pause(200)
@@ -19,5 +12,12 @@ basic.forever(function () {
         basic.pause(200)
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(200)
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() < 80) {
+        pins.servoWritePin(AnalogPin.P2, 180)
+    } else {
+        pins.servoWritePin(AnalogPin.P2, 0)
     }
 })
